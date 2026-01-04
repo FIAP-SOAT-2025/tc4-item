819c48f038be07431de75a4522b37d049e0e6157
fealvesdev <alves.felipeteixeira@gmail.com>
2026-01-04 12:20:33 -0300

    Update k8s-service.tf

--- terraform/k8s-service.tf
+++ terraform/k8s-service.tf
@@ -18,4 +18,24 @@ resource "kubernetes_service" "api_service" {
     type = "LoadBalancer"
   }
   depends_on = [kubernetes_namespace.lanchonete_ns]
+
+}
+
+resource "kubernetes_service" "api_service_internal" {
+  metadata {
+    name      = "api-service-internal"
+    namespace = "tc4-item"
+  }
+  spec {
+    selector = {
+      app = "tc4-item-api"
+    }
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 3000
+    }
+    type = "ClusterIP"
+  }
+  depends_on = [kubernetes_namespace.lanchonete_ns]
 }
